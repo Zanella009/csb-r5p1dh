95298c0b1b29cd25fece587270f684ae5ef95a1a
Code Sandbox <hello@codesandbox.io>
2022-05-04 01:54:29 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ae65b241bda9b31ecbc1f596b41c7fa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92753f8296a84b224fcc",
+    "url": "https://zanella009.github.io/csb-r5p1dh/static/css/main.39811c76.chunk.css"
+  },
+  {
+    "revision": "92753f8296a84b224fcc",
+    "url": "https://zanella009.github.io/csb-r5p1dh/static/js/main.92753f82.chunk.js"
+  },
+  {
+    "revision": "13819aedd59acc74ca80",
+    "url": "https://zanella009.github.io/csb-r5p1dh/static/js/1.13819aed.chunk.js"
+  },
+  {
+    "revision": "edc776ba9587cf081614",
+    "url": "https://zanella009.github.io/csb-r5p1dh/static/js/runtime~main.edc776ba.js"
+  },
+  {
+    "revision": "9c3659e6d6bfc46319af9b188e240078",
+    "url": "https://zanella009.github.io/csb-r5p1dh/index.html"
+  }
+];
